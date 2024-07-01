6ea4e85a141859deb156a9c4d6601fd781bed9d1
mgm67671 <mgm0489@mavs.uta.edu>
2024-07-01 14:15:01 -0500

    Added some sound stuff, not ready for testing yet

---
+++ C++ Code/sound_test.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <unordered_map>
+
+int main()
+{
+    std::string folder = "Sound Samples"; 
+    std::vector<std::string> soundFiles = {"C3.wav", "D3.wav", "E3.wav", "F3.wav", "G3.wav", "A3.wav", "B3.wav", "C4.wav"}; 
+}
