535e88a3487028e2d8181b41ac2ac2bf93e43e27
mgm67671 <mgm0489@mavs.uta.edu>
2024-08-22 16:28:15 -0500

    Adjust test file

--- Code/Play_All_Notes.py
+++ Code/Play_All_Notes.py
@@ -6,7 +6,8 @@ import pygame
 pygame.mixer.init()
 
 # Path to your directory containing the .wav files
-audio_directory = "Sound Samples/Harp/"
+instrument = "Harp"
+audio_directory = "Sound Samples/" + instrument + "/"
 
 # List of notes in diatonic order from lowest to highest
 note_order = [
@@ -14,7 +15,8 @@ note_order = [
     "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
     "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
     "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
-    "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6"
+    "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+    "C7"
 ]
 
 # Play each note in order
